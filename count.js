9977e3f8ef8afda8eea758a74580eb898b32b8eb
Sinesio Bittencourt <contato@sinesio.com.br>
2016-04-04 16:32:55 -0300

    Update count.js

--- count.js
+++ count.js
@@ -7,6 +7,9 @@ timerApp.controller('CountdownController', ['$scope', '$timeout',
           $scope.count--;
           if ($scope.count) {
               	$timeout($scope.decrement, 1000);
+              	//1s == 1000ms  1m == 60000ms 1h == 3600000ms 
+              	//Font: http://extraconversion.com/pt/tempo/horas
+              
               } 
           if ($scope.count <= 0) {	
              		$scope.count = 0;	
